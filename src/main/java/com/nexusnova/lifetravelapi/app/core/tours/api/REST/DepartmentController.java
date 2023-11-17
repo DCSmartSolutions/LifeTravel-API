7e80c9f56a96e99905d96ecb19b20294e6915437
NicolePT <u201921559@upc.edu.pe>
2023-11-17 16:29:06 -0500

    feat(region): Modified Department Service

--- src/main/java/com/nexusnova/lifetravelapi/app/core/tours/api/REST/DepartmentController.java
+++ src/main/java/com/nexusnova/lifetravelapi/app/core/tours/api/REST/DepartmentController.java
@@ -1,13 +1,34 @@
 package com.nexusnova.lifetravelapi.app.core.tours.api.REST;
 
+import com.nexusnova.lifetravelapi.app.core.tours.domain.model.Department;
+import com.nexusnova.lifetravelapi.app.core.tours.domain.services.RegionQueryService;
+import com.nexusnova.lifetravelapi.app.core.tours.mapper.DepartmentMapper;
+import com.nexusnova.lifetravelapi.app.core.tours.resources.details.DepartmentDetail;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/departments")
 @Api(tags = "Api de Departamentos", consumes = "application/json")
 @CrossOrigin
 public class DepartmentController {
+    private final RegionQueryService regionQueryService;
+    private final DepartmentMapper departmentMapper;
+    public DepartmentController(RegionQueryService regionQueryService,
+                                DepartmentMapper departmentMapper) {
+        this.regionQueryService = regionQueryService;
+        this.departmentMapper = departmentMapper;
+    }
+    @GetMapping("")
+    @Operation(summary = "Get All Departments Name")
+    public List<DepartmentDetail> getAllDepartmentsName() {
+        List<Department> departments = regionQueryService.handle();
+        return departmentMapper.toDetail(departments);
+    }
 }
--- src/main/java/com/nexusnova/lifetravelapi/app/core/tours/domain/repositories/DepartmentRepository.java
+++ src/main/java/com/nexusnova/lifetravelapi/app/core/tours/domain/repositories/DepartmentRepository.java
@@ -13,4 +13,6 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     @Query("select d from Department d where d._deleted = false and d.name like %:departmentName%")
     Optional<Department> findByName(@Param("departmentName") String departmentName);
+
+
 }
