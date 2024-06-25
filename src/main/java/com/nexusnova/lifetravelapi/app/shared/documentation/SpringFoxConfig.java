baf9c7e8309ba8b3fd10c45b9ba2a03fbe9c0ce1
Dominik Mendoza <u20201b980@upc.edu.pe>
2024-06-25 13:53:09 -0500

    chore: add server item for https

--- src/main/java/com/nexusnova/lifetravelapi/app/shared/documentation/SpringFoxConfig.java
+++ src/main/java/com/nexusnova/lifetravelapi/app/shared/documentation/SpringFoxConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +19,7 @@ public class SpringFoxConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://lifetravel-api-production.up.railway.app"))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
@@ -31,11 +33,11 @@ public class SpringFoxConfig {
                         .version("1.0.0")
                         .description("""
                                 Developers:
-                                -Ivan Moran (Ivanovich)
-                                -Nicole Price (nPrice)
-                                -Ryan Sweden (Ryen)
-                                -Edgar Malca (EdGar)
-                                -Dominik Mendoza (Zen)""")
+                                -Oliver Tuesta
+                                -Lucas Ruiz
+                                -Edgar Malca
+                                -Dominik Mendoza
+                                -Jose Ramos""")
                         .termsOfService("https://iot-lifetravel-firebase-token-gen.netlify.app")
                         .license(new License().name("GitHub Organization").url("https://github.com/NexusNova-IOT"))
                         .contact(new Contact()
