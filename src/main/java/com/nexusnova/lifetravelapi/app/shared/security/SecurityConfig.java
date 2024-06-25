db610366cea25382d5c263d9aa68b64089e9d393
Dominik Mendoza <u20201b980@upc.edu.pe>
2024-06-25 13:17:41 -0500

    chore: update cors registry

--- src/main/java/com/nexusnova/lifetravelapi/app/shared/security/SecurityConfig.java
+++ src/main/java/com/nexusnova/lifetravelapi/app/shared/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
